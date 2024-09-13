dcb8f10ed375bf677f98b0a39bf96ab7a805d8ad
olgacode <olgabarlou@gmail.com>
2024-09-13 13:48:04 +0300

    Update run_llava_nshot.py

--- llava/eval/run_llava_nshot.py
+++ llava/eval/run_llava_nshot.py
@@ -62,19 +62,19 @@ def eval_model(args, n_shot_examples: List[Tuple[str, str]]):
     tokenizer, model, image_processor, context_len = load_pretrained_model(
         args.model_path, args.model_base, model_name
     )
-    conv_mode = "llava_v1"
-    # if "llama-2" in model_name.lower():
-    #     conv_mode = "llava_llama_2"
-    # elif "mistral" in model_name.lower():
-    #     conv_mode = "mistral_instruct"
-    # elif "v1.6-34b" in model_name.lower():
-    #     conv_mode = "chatml_direct"
-    # elif "v1" in model_name.lower():
-    #     conv_mode = "llava_v1"
-    # elif "mpt" in model_name.lower():
-    #     conv_mode = "mpt"
-    # else:
-    #     conv_mode = "llava_v0"
+
+    if "llama-2" in model_name.lower():
+        conv_mode = "llava_llama_2"
+    elif "mistral" in model_name.lower():
+        conv_mode = "mistral_instruct"
+    elif "v1.6-34b" in model_name.lower():
+        conv_mode = "chatml_direct"
+    elif "v1" in model_name.lower():
+        conv_mode = "llava_v1"
+    elif "mpt" in model_name.lower():
+        conv_mode = "mpt"
+    else:
+        conv_mode = "llava_v0"
 
     conv = conv_templates[conv_mode].copy()
     
@@ -91,6 +91,7 @@ def eval_model(args, n_shot_examples: List[Tuple[str, str]]):
             # Load all images (n-shot examples + query image)
             all_image_files = [example[0] for example in n_shot_examples] + [image_file_query]
             images = load_images(all_image_files)
+            print(images.shape)
             image_sizes = [x.size for x in images]
             images_tensor = process_images(
                 images,
